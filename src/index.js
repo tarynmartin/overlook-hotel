f2f0b8858f5953ea06e19fa3d04b4f3cd784bee4
Taryn Martin <taryn.martin919@gmail.com>
2020-07-30 20:42:11 -0600

    Add imports and variable to hold bookings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './User.js';
+import './Manager.js';
+import './Customer.js';
+import './Rooms.js';
+import './Bookings.js';
+import './Users.js';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+let allBookings = [];
