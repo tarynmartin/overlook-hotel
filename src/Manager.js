77c5501cf6962eb556b2ab0c54559f77b26690a3
Taryn Martin <taryn.martin919@gmail.com>
2020-07-31 18:01:56 -0600

    Add imports for testing and create first method

--- src/Manager.js
+++ src/Manager.js
@@ -1,20 +1,31 @@
+import User from '../src/User';
+
 class Manager extends User {
   constructor() {
     super();
     this.userName = 'manager';
     this.freeRooms = 0;
-    this.percentOccupied = '';
+    this.percentOccupied = 0;
     this.totalRevenue = 0;
   }
   availableRooms() {
     // iterate over bookings and return how many unbooked
+    // pull from bookings? or put that in here?
   }
   percentUnavailable() {
     // calculate percent and move to percentOccupied
+    // pull from bookings? or put that in here?
   }
-  calculateRevenue() {
-    // iterate over booked rooms
-    // collect price (reduce?) and add to totalRevenue
+  calculateRevenue(bookings, rooms) {
+    return rooms.reduce((total, room) => {
+      bookings.forEach(booking => {
+        if (room.number === booking.roomNumber) {
+          total = total + room.costPerNight;
+        }
+      })
+      this.totalRevenue = Number.parseFloat(total.toFixed(2));
+      return total;
+    }, 0)
   }
 }
 
