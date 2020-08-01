ccd3aa9bd49130d4c453dfe3c45256d17a48058d
Taryn Martin <taryn.martin919@gmail.com>
2020-08-01 11:28:11 -0600

    Refactor User class

--- src/User.js
+++ src/User.js
@@ -4,11 +4,6 @@ class User {
     this.customer = false;
     this.userName = userName;
     this.password = 'overlook2020';
-    this.pastBookings = [];
-    this.availableRooms = 0;
-    this.unavailableRooms = 0;
-    this.bookingToday = [];
-    this.futureBookings = [];
     this.amountSpent = 0;
   }
   checkUserType(userName) {
@@ -18,26 +13,14 @@ class User {
       this.customer = true;
     }
   }
-  findBookings(date, bookings) {
-    if (this.customer === true) {
-
-    } else {
-
-    }
-    // iterate over all bookings
-    // if userId matches, push booking into array
-    //if date is before today, call findPastBookings
-    // if date matches current date, push to bookingToday
-    // call futureBooking if date is in the future
-  }
-  findPastBookings() {
-    // add new booking to pastBookings array
-    // sort pastBookings array, newest at beginning
-  }
   calculateAmountSpent(date, bookings, rooms) {
+    const inputDate = new Date(date);
+    const userDate = inputDate.getTime();
     return rooms.reduce((total, room) => {
       bookings.forEach(booking => {
-        if (date === booking.date && room.number === booking.roomNumber) {
+        const bookedDate = new Date(booking.date);
+        const bookingDate = bookedDate.getTime();
+        if (userDate === bookingDate && room.number === booking.roomNumber) {
           total = total + room.costPerNight;
         }
       })
