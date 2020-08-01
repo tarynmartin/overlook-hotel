533df7ec6d49a4233e43a09993333513469a68d6
Taryn Martin <taryn.martin919@gmail.com>
2020-08-01 15:38:09 -0600

    Add pseudocode for the future

--- src/index.js
+++ src/index.js
@@ -14,5 +14,10 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 let allBookings = [];
 
+// on load, get users and rooms
 // on login (press submit button), create a new user
-// here, do an if/else statement for user data; if manager = true, instantiate maanger class, else if customer = true, instantiate customer
+// use fetch to get bookings data when submit is pressed
+// here, do an if/else statement for user data; if manager = true, instantiate manager class, else if customer = true, instantiate customer
+// with manager, input today's date and bookings when creating it
+
+//pull userID from userName here to create customer?
