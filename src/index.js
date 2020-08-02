3a7271d4e5aa47f518b59b19c314f9bc7974f7b2
Taryn Martin <taryn.martin919@gmail.com>
2020-08-02 14:00:22 -0600

    Create functions to display site on load and show correct page based on login information #28

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import './get-api.js';
 import './User.js';
@@ -7,24 +6,44 @@ import './Customer.js';
 import './Rooms.js';
 import './Bookings.js';
 import './Users.js';
+import getOnLoad from './get-api.js';
+const moment = require('moment');
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import getOnLoad from './get-api.js';
+import User from './User.js';
+import Manager from './Manager.js';
+import Customer from './Customer.js';
 
-console.log('This is the JavaScript entry file - your code begins here.');
-
+let user;
+let manager;
+let customer;
 let allBookings = [];
 let users = [];
 let rooms = [];
+let userName;
+let password;
+let currentDate = moment().format('YYYY/MM/DD');
 
-// on load, get users and rooms
-// on login (press submit button), create a new user
-// use fetch to get bookings data when submit is pressed
-// here, do an if/else statement for user data; if manager = true, instantiate manager class, else if customer = true, instantiate customer
-// with manager, input today's date and bookings when creating it
+const submitBtn = document.querySelector(".psw-submit");
+const userNameInput = document.querySelector(".username");
+const passwordInput = document.querySelector(".password");
+const searchBar = document.querySelector(".search-bar");
+const makeBookingBar = document.querySelector(".select-date");
+const managerDashboard = document.querySelector(".manager");
+const customerDashboard = document.querySelector(".customer");
+const landingPage = document.querySelector(".welcome");
+const managerSearchResults = document.querySelector(".manager-search");
+const customerMakeBooking = document.querySelector(".customer-booking");
+const totalRoomsAvailableToday = document.querySelector(
+  ".number-rooms-available"
+);
+const percentRoomsOccupiedToday = document.querySelector(".percent-occupied");
+const totalRevenueToday = document.querySelector(".total-revenue");
 
-//pull userID from userName here to create customer?
+submitBtn.addEventListener("click", createUser);
+userNameInput.addEventListener("input", collectUserName);
+passwordInput.addEventListener("input", collectPassword);
 
 getOnLoad().then(allData => {
   allData.users.forEach(user => {
@@ -42,9 +61,47 @@ function loadPage() {
   console.log(rooms);
 }
 
-const submitBtn = document.querySelector('.psw-submit');
+function collectUserName(event) {
+  userName= event.target.value;
+}
 
-submitBtn.addEventListener('click', getOnClick);
+function collectPassword(event) {
+  password = event.target.value;
+}
+
+function createUser() {
+  getOnClick();
+  user = new User(currentDate, userName);
+  let checkUser = user.checkUserType(userName, currentDate, allBookings, rooms);
+  console.log(user.id);
+  if (checkUser === 'manager' && user.password === password) {
+    manager = new Manager(currentDate, userName, allBookings, rooms);
+    landingPage.classList.add('hide');
+    showManagerDashboard();
+  } else if (checkUser === 'customer' && user.password === password) {
+    customer = new Customer(currentDate, userName, users[user.id - 1]);
+    landingPage.classList.add("hide");
+    showCustomerDashboard(); 
+  } else {
+    alert('Please enter a valid username and password')
+  }
+}
+
+function showManagerDashboard() {
+  user.calculateAmountSpent(currentDate, allBookings, rooms);
+  manager.availableRooms();
+  manager.percentUnavailable();
+  searchBar.classList.remove('hide');
+  managerDashboard.classList.remove('hide');
+  totalRoomsAvailableToday.innerText = manager.numberOfAvailableRooms;
+  percentRoomsOccupiedToday.innerText = `${manager.percentageUnavailable}%`;
+  totalRevenueToday.innerText = `$${user.totalRevenue}`;
+}
+
+function showCustomerDashboard() {
+  makeBookingBar.classList.remove('hide');
+  customerDashboard.classList.remove('hide');
+}
 
 function findAllBookings() {
   const promise = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
