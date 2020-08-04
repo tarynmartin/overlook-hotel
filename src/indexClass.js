930f2408d3ce6d1ecd7ac8ba6e3bbe1c1cfc9469
Taryn Martin <taryn.martin919@gmail.com>
2020-08-04 09:46:46 -0600

    Remove fetch class to test in separate file #39

--- src/indexClass.js
+++ src/indexClass.js
@@ -15,33 +15,6 @@ class Index {
   }
 }
 
-// class Fetch {
-//   constructor() {
-//   }
-  function fetchUserData() {
-    return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
-      .then(response => response.json())
-      .then(data => data.users)
-      .catch(error => console.log(error));
-  }
 
-  async function fetchRoomData() {
-    return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
-      .then(response => response.json())
-      .then(data => data.rooms)
-      .catch(error => console.log(error));
-  }
 
-  async function getOnLoad() {
-    return Promise.all([fetchUserData(), fetchRoomData()])
-      .then(response => {
-        let fetchData = {};
-        fetchData.users = response[0];
-        fetchData.rooms = response[1];
-        return fetchData;
-      })
-      .catch(error => console.log(error));
-  }
-// }
-
-export default Index; fetchUserData;
+export default Index;
