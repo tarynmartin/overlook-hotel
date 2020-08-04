31df9ec385bfee7b4c3d0ddd6df684c8c50be2ae
Taryn Martin <taryn.martin919@gmail.com>
2020-08-04 09:46:25 -0600

    Add separate file for testing fetch get #39

---
+++ test/get-api-test.js
@@ -0,0 +1,55 @@
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+import "../src/get-api-class.js";
+import Fetch from "../src/get-api-class.js";
+chai.use(spies);
+
+describe("Fetch", function () {
+  let fetchResponse;
+  let fetchObject;
+  beforeEach(() => {
+    fetchResponse = {
+      then: () => {
+        return fetchResponse;
+      },
+      then: () => {
+        return fetchResponse;
+      },
+      catch: () => {
+        return fetchResponse;
+      },
+    };
+    fetchObject = new Fetch();
+    global.server = {};
+    global.document = {};
+    global.fetch = () => {
+      return fetchResponse;
+    };
+    chai.spy.on(global, ["fetch"]);
+  });
+  it("should be a function", function () {
+    expect(Fetch).to.be.a("function");
+  });
+  it("should create an instance of fetch", function () {
+    expect(fetchObject).to.be.an.instanceof(Fetch);
+  });
+  it("fetchUserData should return an object", function () {
+    let finishedFetch = fetchObject.fetchUserData();
+    let fetchResponseType = typeof finishedFetch;
+
+    expect(fetchResponseType).to.equal("object");
+  });
+  it("fetchRoomData should return an object", function () {
+    let finishedFetch = fetchObject.fetchRoomData();
+    let fetchResponseType = typeof finishedFetch;
+
+    expect(fetchResponseType).to.equal("object");
+  });
+  it("getOnLoad should return an object", function () {
+    let finishedFetch = fetchObject.getOnLoad();
+    let fetchResponseType = typeof finishedFetch;
+
+    expect(fetchResponseType).to.equal("object");
+  });
+});
